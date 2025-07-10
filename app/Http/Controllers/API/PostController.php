a820c802f76954ce90f082d5137ab20f5a4dd2ef
Md Mazharul Haque Emon <mazharulhaqueemon.com>
2025-07-10 12:53:06 +0600

    Post api with post ,get,update,delete

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -3,24 +3,43 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PostRequest;
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Http\Controllers\API\BaseController as BaseController;
 
-class PostController extends Controller
+class PostController extends BaseController
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $data['posts']= Post::all();
+        return $this->sendResponse($data, 'All posts data');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(PostRequest $request)
     {
-        //
+
+
+        $img = $request->image;
+        $imageName = time().'_'.$img->getClientOriginalName();
+        $path= $img->storeAs('images', $imageName, 'public');
+
+
+
+        $post = Post::create([
+             'title' => $request->title,
+             'description' => $request->description,
+             'image'=>$imageName,
+         ]);
+
+
+        return $this->sendResponse($post, 'Post created successfully');
     }
 
     /**
@@ -28,22 +47,65 @@ class PostController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $data['post']= Post::select(
+            'id',
+            'title',
+            'description',
+            'image',
+
+        )->where(['id' => $id])->get();
+
+    return $this->sendResponse($data, 'Post retrieved successfully');
+
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(PostRequest $request, string $id)
     {
-        //
+         $post = Post::findOrFail($id);
+
+          if($request->hasFile('image')){
+            // previous image or file delete
+            $image_path = public_path('storage/images/'.$post->image);
+            if(file_exists($image_path)){
+                unlink($image_path);
+            }
+            $img = $request->image;
+            $imageName = time().'_'.$img->getClientOriginalName();
+            $path= $img->storeAs('images', $imageName, 'public');
+
+          }
+          else{
+            $imageName=$post->image;
+          }
+
+        $post->update([
+             'title' => $request->title,
+             'description' => $request->description,
+             'image'=>$imageName,
+         ]);
+
+        return $this->sendResponse($post, 'Post updated successfully');
     }
+
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
     {
-        //
+
+        $post = Post::findOrFail($id);
+
+        $image_path = public_path('storage/images/'.$post->image);
+        if(file_exists($image_path)){
+            unlink($image_path);
+        }
+
+        $post->delete();
+
+        return $this->sendResponse([], 'Post deleted successfully');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('signup',[AuthController::class,'signup']);
 Route::post('login',[AuthController::class,'login']);
-Route::post('logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout',[AuthController::class,'logout']);
+    Route::apiResource('posts',PostController::class);
+});
+
+
